acfe832d0bd6a17600d9735f6f85189a8b780d35
conglynguyen <love12a2.ucoz.com@gmail.com>
2014-05-28 23:20:52 +0700

    Update demo.java

--- demo/src/demo/demo.java
+++ demo/src/demo/demo.java
@@ -5,6 +5,10 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		
+		
+		
+		System.out.println("sua lan 1");
 		System.out.println("Hello");
 	}
 
